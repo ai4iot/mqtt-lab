4fa17d70e0dfe1dbebeed60a6b0fec7cf918d635
Alejandro Martín <91533700+msalexms@users.noreply.github.com>
2023-11-30 17:36:57 +0100

    Update publisher.ino
    Adding initial code for publisher

--- publisher/publisher/publisher.ino
+++ publisher/publisher/publisher.ino
@@ -5,13 +5,17 @@ const char * ssid = "ASUS_GRAM";
 const char * password = "GramLab0rat0ri0";
 
 
-const char * broker_ip = "192.168.77.106"
+const char * broker_ip = "192.168.77.106";
+const uint16_t mqtt_port = 1883;
 
 WiFiClient esp_client;
 
 PubSubClient mqtt_client(esp_client);
 
 void setup_wifi();
+void init_mqtt();
+void connect_mqtt();
+void handdle_qmtt();
 
 
 void setup() {
@@ -21,9 +25,12 @@ void setup() {
 
   setup_wifi();
 
-  Serial.println("Connected to WiFi");
+  init_mqtt();
 
+  connect_mqtt();
   
+
+  Serial.println("\n Ok");
 
 
 
@@ -33,7 +40,46 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+void init_mqtt(){
 
+  Serial.print("Connect to MQTT -");
+  Serial.print(broker_ip);
+  Serial.print("-");
+  Serial.println(mqtt_port);
+
+  mqtt_client.setServer(broker_ip, mqtt_port);
+}
+
+void connect_mqtt(){
+
+  while(!mqtt_client.connected()){
+
+    Serial.print("Connecting to MQTT");
+
+    if (mqtt_client.connect("ESP32Client"))
+           Serial.println("connected");
+       
+    else{   
+           Serial.print("failed with state ");
+           Serial.print(mqtt_client.state());
+           delay(500);
+       }
+  }
+
+
+}
+
+void handdle_qmtt(){
+
+  if(!mqtt_client.connected()){
+
+    connect_mqtt();
+
+  }
+
+  mqtt_client.loop();
+
+}
 
 void setup_wifi(){
 
@@ -45,5 +91,6 @@ void setup_wifi(){
           Serial.print(".") ;
        }
 
+  Serial.println("Connected to WiFi");
 
 }
